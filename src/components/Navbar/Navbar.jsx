2f5e804f38773213c4a9c2c7851095a2d0833a6a
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-10-19 20:25:03 -0500

    modificacion pagina inicio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,45 +2,181 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { createTheme } from '@mui/material/styles';
+import { pink } from "@mui/material/colors";
+import { ThemeProvider } from "@mui/material/styles";
+import { createMuiTheme } from "@mui/material/styles";
 
-function Navbar() {
-  // Subscribe to the AuthContext to gain access to
-  // the values from AuthContext.Provider's `value` prop
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+const theme = createMuiTheme({
+	palette:{
+		primary:{
+			main:'#ffc5c9'
+		},
+		secondary:{
+			main: '#212121'
+		}
+	}
+})
 
-  return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-      {isLoggedIn && (
-        <>
-          <button onClick={logOutUser}>Logout</button>
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-          <Link to="/profile">
-            <button>Profile</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
-          </Link>
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-          <span>{user && user.name}</span>
-        </>
-      )}
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-    </nav>
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+	return (
+		<ThemeProvider theme={theme}>
+    <AppBar position="static" className="disabled" color="primary">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+           <img className="beauty-nav" src="images/beauty-blanco.png" alt="" />
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            {/* <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip> */}
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+		</ThemeProvider>
   );
-}
-
-export default Navbar;
+};
+export default ResponsiveAppBar;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,28 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div>
-      <h1>Home page</h1>
-    </div>
+      <h1 className="bienvenidx">Bienvenidx a</h1>
+			<span className="beautyymx">Beautyy Mx</span>
+			<hr />
+			<h2 className="titulo-conocenos">Conocenos!</h2>
+		 <div className="conocenos">
+			<img className="conocenos-o" src="images/beauty-ico.png" alt="" />
+			<span className="conocenos-t">Somos una empresa Mexicana dedicada a la venta de maquillaje y skincare de importación. Todos nuestros productos son 100% originales, ofreciendo una variedad de marcas en tendencia, de calidad y nuevas para su compra al por menor dentro de México.</span>
+		 </div>
+		 <hr />
+			<h2>Productos</h2>
+		 <div className="productos">
+			<img className="productosCarreta" src="images/productos-carreta.jpeg" alt="" />
+			<span>Encuentra con nosotros los últimos lanzamientos del mundo del maquillaje. Contamos con la mejor calidad y variedad en el estado de Morelos.</span>
+		 </div>
+		 <hr />
+		 <div className="puntos">
+			<h2>Puntos de Venta</h2>
+			<img className="carreta" src="images/carreta.jpeg" alt="" />
+			<span>Nos puedes encontrar en diferentes puntos dentro de Cuernavaca,Morelos.</span>
+		 </div>
+		</div>
+    
   );
 }
 
