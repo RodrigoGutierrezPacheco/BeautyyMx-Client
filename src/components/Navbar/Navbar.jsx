f9526010c1db8a803ae294be8f00b829d04c835a
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-11-29 18:12:23 -0600

    modificacion navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,22 +16,22 @@ function CollapsibleExample() {
           <Nav className="me-auto">
             <Nav.Link style={{color:'black',fontWeight:'500' }} href="/">Inicio</Nav.Link>
             <Nav.Link style={{color:'black',fontWeight:'500' }} href="#pricing">¿Qiuienes somos?</Nav.Link>
-            <NavDropdown color='black' title="Productos" id="collasible-nav- black">
+            <NavDropdown className='black' title="Productos" id="black">
               <NavDropdown.Item style={{color:'black',fontWeight:'500' }} href="#action/3.1">Rostro</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item  style={{color:'black',fontWeight:'500' }} href="#action/3.2">
                 Ojos
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Labios</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item style={{color:'black',fontWeight:'500' }} href="#action/3.3">Labios</NavDropdown.Item>
+              <NavDropdown.Item style={{color:'black',fontWeight:'500' }} href="#action/3.4">
                 Paletas
               </NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.4">
+							<NavDropdown.Item style={{color:'black',fontWeight:'500' }} href="#action/3.4">
                 Cuidado de la Piel
               </NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.4">
+							<NavDropdown.Item style={{color:'black',fontWeight:'500' }} href="#action/3.4">
                 Tamaño de Viaje
               </NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.4">
+							<NavDropdown.Item style={{color:'black',fontWeight:'500' }} href="#action/3.4">
                 Accesorios
               </NavDropdown.Item>
             </NavDropdown >
