3a24825691d29719c3294776e41480ecdd9d7490
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-03-10 13:40:45 -0600

    modificacion navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -38,6 +38,43 @@ function CollapsibleExample() {
 		setIsOpen(false);
 	}
 
+	function loNuevo(){
+		window.location.href="/lo-nuevo"
+	}
+	function loMasVendido(){
+		window.location.href="/lo-mas-vendido"
+	}
+	function maquillaje(){
+		window.location.href="/maquillaje"
+	}
+	function cuidadoParaLaPiel(){
+		window.location.href="/cuidado-para-la-piel"
+	}
+	function descuentos(){
+		window.location.href="/descuentos"
+	}
+	function labios(){
+		window.location.href="/labios"
+	}
+	function minis(){
+		window.location.href="/minis"
+	}
+	function ojos(){
+		window.location.href="/ojos"
+	}
+	function paletas(){
+		window.location.href="/paletas"
+	}
+	function rosto(){
+		window.location.href="/rostro"
+	}
+	function todosLosProductos(){
+		window.location.href="/todos-los-productos"
+	}
+	function puntosDeVenta(){
+		window.location.href="/puntos-de-venta"
+	}
+
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
     subtitle.style.color = '#f00';
@@ -62,18 +99,18 @@ function CollapsibleExample() {
 				</div>
         <div className='white textoModal'>
 				<div start1>
-          <h1 className='textoModal'>Lo Nuevo</h1>
-          <h1 className='textoModal'>Lo Mas Vendido</h1>
-          <h1 className='textoModal'>Maquillaje</h1>
-          <h1 className='textoModal'>Cuidado de la Piel</h1>
-          <h1 className='textoModal'>Descuentos</h1>
-          <h1 className='textoModal'>Labios</h1>
-          <h1 className='textoModal'>Minis</h1>
-          <h1 className='textoModal'>Ojos</h1>
-          <h1 className='textoModal'>Paletas</h1>
-          <h1 className='textoModal'>Rostro</h1>
-          <h1 className='textoModal'>Todos los productos</h1>
-          <h1 className='textoModal'>Puntos de Venta</h1>
+          <h1 onClick={loNuevo} className='textoModal'>Lo Nuevo</h1>
+          <h1 onClick={loMasVendido} className='textoModal'>Lo Mas Vendido</h1>
+          <h1 onClick={maquillaje} className='textoModal'>Maquillaje</h1>
+          <h1 onClick={cuidadoParaLaPiel} className='textoModal'>Cuidado de la Piel</h1>
+          <h1 onClick={descuentos} className='textoModal'>Descuentos</h1>
+          <h1 onClick={labios} className='textoModal'>Labios</h1>
+          <h1 onClick={minis} className='textoModal'>Minis</h1>
+          <h1 onClick={ojos} className='textoModal'>Ojos</h1>
+          <h1 onClick={paletas} className='textoModal'>Paletas</h1>
+          <h1 onClick={rosto} className='textoModal'>Rostro</h1>
+          <h1 onClick={todosLosProductos} className='textoModal'>Todos los productos</h1>
+          <h1 onClick={puntosDeVenta} className='textoModal'>Puntos de Venta</h1>
 				</div>
         </div>
 			</div>
