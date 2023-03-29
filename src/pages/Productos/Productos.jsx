1454702b52dd330e70724daefe1c2beb9f4f4666
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-03-28 21:52:55 -0600

    prueba vista de todos los productos, prueba on axios

--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -14,6 +14,7 @@ import { motion } from 'framer-motion';
 import { style } from '@mui/system';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 export default function Productos() {
 	const [data,setData] = useState([]);
@@ -22,111 +23,134 @@ export default function Productos() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+	const [pageNumber, setPageNumber] = useState(0);
+
+  const productsPerPage = 16;
+  const pagesVisited = pageNumber * productsPerPage;
+
+	const pageCount = Math.ceil(data.length / productsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+	const displayProducts = data
+	.slice(pagesVisited, pagesVisited + productsPerPage)
+	.map(item => (
+		<div className="boxProduct" key={item.codigo}>
+			<img className="producto1" src="images/product1.jpg" alt="" />
+			<p className='marca'>{item.marca}</p>
+			<p className='descripcion'>Descripcion: <br />{item.descripcion}</p>
+			<h3 className="precio">${item.precio}.00MXN</h3>
+			<p>{item.codigo}</p>
+			<motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button>
+		</div>
+	));
 
 
 
- const maquillaje = [
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00",
-	},
-	{
-		marca:"Marca 2",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00",
-		now : 20
-	},
-	{
-		marca:"Marca 3",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00",
-		now : 30
-	},
-	{
-		marca:"Marca 4",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00",
-		now : 40
-	},
-	{
-		marca:"Marca 5",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00",
-		now : 50
-	},
- ]
+//  const maquillaje = [
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00",
+// 	},
+// 	{
+// 		marca:"Marca 2",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00",
+// 		now : 20
+// 	},
+// 	{
+// 		marca:"Marca 3",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00",
+// 		now : 30
+// 	},
+// 	{
+// 		marca:"Marca 4",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00",
+// 		now : 40
+// 	},
+// 	{
+// 		marca:"Marca 5",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00",
+// 		now : 50
+// 	},
+//  ]
 
- const cuidadoDeLaPiel = [
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
- ]
+//  const cuidadoDeLaPiel = [
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+//  ]
 
- const accesorios = [
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
-	{
-		marca:"Marca 1",
-		nombre:"Nombre 1",
-		descripcion:"Descripcion 1",
-		precio:"$100.00"
-	},
- ]
+//  const accesorios = [
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+// 	{
+// 		marca:"Marca 1",
+// 		nombre:"Nombre 1",
+// 		descripcion:"Descripcion 1",
+// 		precio:"$100.00"
+// 	},
+//  ]
 
  function maquillaje1(){
 	window.location.href="/maquillaje"
@@ -141,7 +165,7 @@ export default function Productos() {
  }
  
  useEffect(()=>{
-	axios.get('https://gist.githubusercontent.com/RodrigoGutierrezPacheco/6fdcbaee593f135f4d9a062bfeba3de7/raw/9071a0b320abab45a4b5c88e3b96cb1750488483/beautyymx.json')
+	axios.get('https://gist.githubusercontent.com/RodrigoGutierrezPacheco/6fdcbaee593f135f4d9a062bfeba3de7/raw/34c3117c7be90c9a01daf4ebb1c1dbf4cf1e4b90/beautyymx.json')
 	.then(response => {
 		setData(response.data)
 		console.log(response.data)
@@ -152,20 +176,27 @@ export default function Productos() {
 },[]);
 
   return (
-		
 		<div>
 			<img className='portada' src="images/productos.png" alt="" />
-			{/* {data.filter(obj => obj.Marca==="The Ordinary"||"BT002").map(item =>(
-				<div className="boxProduct">
-				<img className="producto1" src="images/product1.jpg" alt="" />
-				<h1>{item.Marca}</h1>
-				<h3>{item.Descripcion}</h3>
-				<h3 className="precio">${item.Precio}.00MXN</h3>
-				<p>{item.Código}</p>
-				<motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button>
-			 </div>
-			))} */}
-		 <div className='boxVistaRapida1'>
+		  <div className='boxVistaRapida'>
+      {displayProducts}
+			<ReactPaginate
+       previousLabel={<img src="https://cdn-icons-png.flaticon.com/128/151/151846.png" alt="Siguiente" className='siguiente'/>}
+			 nextLabel={<img src="https://cdn-icons-png.flaticon.com/128/271/271228.png" alt="Siguiente" className='siguiente'/>}       
+			 previousClassName={"boton-anterior"}
+       nextClassName={"boton-siguiente"}
+       onPageChange={changePage}
+       pageCount={pageCount}
+       containerClassName={"pagination"}
+       subContainerClassName={"pages pagination"}
+       activeClassName={"active"}
+			 currentPageClassName={"paginaAcutal"}
+			 pageRangeDisplayed={false}
+       marginPagesDisplayed={false}			 
+			 />
+
+    </div>
+		 {/* <div className='boxVistaRapida1'>
 			<h1 className="title red">Maquillaje</h1>
 			<Carousel interval={30000} indicators={false} variant="dark">
 			{maquillaje.map((lanzamiento)=>
@@ -175,16 +206,15 @@ export default function Productos() {
 					 <h1>{lanzamiento.marca}</h1>
 					 <h3>NOMBRE DEL PRODUCTO</h3>
 					 <h3 className="precio">$799.00MXN</h3>
-					 {/* <h3>Codigo de producto: BT001</h3> */}
 					 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button>
 					</div>
 				</Carousel.Item>
        )}
      </Carousel>
 		 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1 mt" onClick={maquillaje1}>Ver Todo en Maquillaje</motion.button>
-		 </div>
+		 </div> */}
 
-		 <div className='boxVistaRapida1'>
+		 {/* <div className='boxVistaRapida1'>
 			<h1 className="title red mt">Cuidado de la Piel</h1>
 			<Carousel interval={3000} indicators={false} variant="dark">
 			{maquillaje.map((lanzamiento)=>
@@ -194,16 +224,15 @@ export default function Productos() {
 					 <h1>{lanzamiento.marca}</h1>
 					 <h3>NOMBRE DEL PRODUCTO</h3>
 					 <h3 className="precio">$799.00MXN</h3>
-					 {/* <h3>Codigo de producto: BT001</h3> */}
 					 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button>
 					</div>
 				</Carousel.Item>
        )}
      </Carousel>
 		 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1 mt" onClick={cuidadoDeLaPiel1} >Ver Todo Cudiado de la Piel</motion.button>
-		 </div>
+		 </div> */}
 
-		 <div className='boxVistaRapida1'>
+		 {/* <div className='boxVistaRapida1'>
 			<h1 className="title red mt">Accesorios</h1>
 			<Carousel interval={3000} indicators={false} variant="dark">
 			{maquillaje.map((lanzamiento)=>
@@ -213,14 +242,13 @@ export default function Productos() {
 					 <h1>{lanzamiento.marca}</h1>
 					 <h3>NOMBRE DEL PRODUCTO</h3>
 					 <h3 className="precio">$799.00MXN</h3>
-					 {/* <h3>Codigo de producto: BT001</h3> */}
 					 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button>
 					</div>
 				</Carousel.Item>
        )}
      </Carousel>
 		 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1 mt" onClick={accesorios1}>Ver Todo en Accesorios</motion.button>
-		 </div>
+		 </div> */}
 
 		</div>
   );
