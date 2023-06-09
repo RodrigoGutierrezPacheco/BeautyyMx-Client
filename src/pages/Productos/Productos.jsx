4b5a26a0342b5272b4b0673c93a006db1e375fc8
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-06-09 14:42:25 -0600

    modificacion vista productos

--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -15,21 +15,15 @@ import axios from 'axios';
 import ReactPaginate from 'react-paginate';
 
 export default function Productos() {
-
-  const [selectedProduct, setSelectedProduct] = useState(null); // Nuevo estado para almacenar el producto seleccionado
-
+  const [selectedProduct, setSelectedProduct] = useState(null);
   const [data, setData] = useState([]);
   const [open, setOpen] = useState(false);
   const [pageNumber, setPageNumber] = useState(0);
-  const productsPerPage = 10;
-  const pagesVisited = pageNumber * productsPerPage;
   const [searchTerm, setSearchTerm] = useState('');
   const [brandFilter, setBrandFilter] = useState('');
-  const [priceFilter, setPriceFilter] = useState('');
-  const [productFilter, setProductFilter] = useState('');
 
-	const handleOpen = (product) => { // Modificamos la función handleOpen para recibir el producto seleccionado
-    setSelectedProduct(product); // Almacenamos el producto seleccionado en el estado
+  const handleOpen = (product) => {
+    setSelectedProduct(product);
     setOpen(true);
   };
 
@@ -48,44 +42,39 @@ export default function Productos() {
       });
   }, []);
 
-	const filteredProducts = data ? data.filter(item => {
-		const lowerCaseSearchTerm = searchTerm.toLowerCase();
-		const lowerCaseBrandFilter = brandFilter.toLowerCase();
-		const lowerCaseProductFilter = productFilter.toLowerCase();
-	
-		const matchesSearchTerm = item.descripcion && item.descripcion.toLowerCase().includes(lowerCaseSearchTerm);
-		const matchesBrandFilter = item.marca && item.marca.toLowerCase() === lowerCaseBrandFilter || lowerCaseBrandFilter === '';
-		const matchesPriceFilter = item.precio && parseInt(item.precio) <= parseInt(priceFilter) || priceFilter === '';
-		const matchesProductFilter = item.producto && item.producto.toLowerCase() === lowerCaseProductFilter || lowerCaseProductFilter === '';
-	
-		return matchesSearchTerm && matchesBrandFilter && matchesPriceFilter && matchesProductFilter;
-	}) : [];
-	
-  const pageCount = Math.ceil(filteredProducts.length / productsPerPage);
-  const pageCountFiltered = Math.ceil(filteredProducts.length / productsPerPage);
+  const filteredProducts = data ? data.filter(item => {
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    const lowerCaseBrandFilter = brandFilter.toLowerCase();
+
+    const matchesSearchTerm = item.descripcion && item.descripcion.toLowerCase().includes(lowerCaseSearchTerm);
+    const matchesBrandFilter = item.marca && item.marca.toLowerCase() === lowerCaseBrandFilter || lowerCaseBrandFilter === '';
+
+    return matchesSearchTerm && matchesBrandFilter;
+  }) : [];
+
+  const pageCountFiltered = Math.ceil(filteredProducts.length / filteredProducts.length);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
 
   const displayProducts = filteredProducts
-    .slice(pagesVisited, pagesVisited + productsPerPage)
     .map(item => (
       <div className="boxProduct" key={item.codigo}>
-				<div className="contenedor-imagen">
-        	<img onClick={() => handleOpen(item)} className="imagen-producto" src={`https://drive.google.com/uc?export=view&id=${item.id}`} alt="imagen del producto" />
-				</div>
+        <div className="contenedor-imagen">
+          <img onClick={() => handleOpen(item)} className="imagen-producto" src={`https://drive.google.com/uc?export=view&id=${item.id}`} alt="imagen del producto" />
+        </div>
         <p className="descripcion marginl marginr">
           {item.marca}
         </p>
-				<p className='contenedor-descripcion marginr marginl'>{item.descripcion}</p>
+        <p className='contenedor-descripcion marginr marginl'>{item.descripcion}</p>
         <h3 className="precio">${item.precio}.00MXN</h3>
         <p className="subtitle">{item.codigo} - {item.contenido}</p>
-				<motion.button
+        <motion.button
           whileTap={{ scale: 1.2 }}
           whileHover={{ scale: 1.1 }}
           className="button1"
-          onClick={() => handleOpen(item)} // Pasamos el producto seleccionado a la función handleOpen al hacer clic en el botón
+          onClick={() => handleOpen(item)}
         >
           Ver Producto
         </motion.button>
@@ -95,13 +84,13 @@ export default function Productos() {
   return (
     <div>
       <img className="portada" src="images/productos.png" alt="" />
-			<div className='flex marginb'>
-				<h1>Encontrar por Marca</h1>
+      <div className='flex marginb'>
+        <h1>Encontrar por Marca</h1>
         <select className='filtro' value={brandFilter} onChange={e => setBrandFilter(e.target.value)}>
           <option value="">Todas las marcas</option>
-					<option value="Anastasia Beverly Hills">Anastasia Beverly Hills</option>
+          <option value="Anastasia Beverly Hills">Anastasia Beverly Hills</option>
           <option value="AOA">AOA</option>
-					<option value="Benefit">Benefit</option>
+          <option value="Benefit">Benefit</option>
           <option value="Bissú">Bissú</option>
           <option value="Carolina Herrera">Carolina Herrera</option>
           <option value="Cerave">Cerave</option>
@@ -110,7 +99,7 @@ export default function Productos() {
           <option value="Danessa Myricks">Danessa Myricks</option>
           <option value="Diamond Beauty">Diamond Beauty</option>
           <option value="E.L.F">E.L.F</option>
-					<option value="Fenty Beauty">Fenty Beauty</option>
+          <option value="Fenty Beauty">Fenty Beauty</option>
           <option value="Glam Glow">Glam Glow</option>
           <option value="Glossier">Glossier</option>
           <option value="Good Molecules">Good Molecules</option>
@@ -121,7 +110,7 @@ export default function Productos() {
           <option value="Kiko Milano">Kiko Milano</option>
           <option value="Klean Color">Klean Color</option>
           <option value="L´Oreal">L´Oreal</option>
-					<option value="Liquid Shadow Jeffree Star">Liquid Shadow Jeffree Star</option>
+          <option value="Liquid Shadow Jeffree Star">Liquid Shadow Jeffree Star</option>
           <option value="Made by Mitchell">Made by Mitchell</option>
           <option value="Mario Badesc">Mario Badescu</option>
           <option value="Maybelline">Maybelline</option>
@@ -133,7 +122,7 @@ export default function Productos() {
           <option value="P Louise">P Louise</option>
           <option value="Patrick Ta Beauty">Patrick Ta Beauty</option>
           <option value="Paula´s Choice">Paula´s Choice</option>
-					<option value="Pears">Pears</option>
+          <option value="Pears">Pears</option>
           <option value="Peripera">Peripera</option>
           <option value="Physician´s Formula">Physician´s Formula</option>
           <option value="Prosa">Prosa</option>
@@ -149,61 +138,47 @@ export default function Productos() {
           <option value="The Ordinary">The Ordinary</option>
           <option value="Tintaline Bissú labios">Tintaline Bissú labios</option>
           <option value="Too Faced">Too Faced</option>
-					<option value="Urban Decay">Urban Decay</option>
+          <option value="Urban Decay">Urban Decay</option>
           <option value="Wedding Proof Spray Jeffree">Wedding Proof Spray Jeffree</option>
           <option value="Wet n Wild">Wet n Wild</option>
-          {/* <option value=""></option> */}
-          {/* Agrega más opciones de marcas aquí */}
         </select>
-				</div>
-				<h1 className='margint'>{brandFilter ? brandFilter : "Todas las marcas"}</h1>      
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-          placeholder="Buscar tus productos..."
-					className='buscador'
-        />
-				<div className="column">
-
-
-        {/* <input
-          type="number"
-          value={priceFilter}
-          onChange={e => setPriceFilter(e.target.value)}
-          placeholder="Filtrar por precio máximo"
-        /> */}
-
-         {/* <select value={productFilter} onChange={e => setProductFilter(e.target.value)}>
-          <option value="">Todos los productos</option>
-        </select>  */}
-				<div className='boxVistaRapida marginr marginl'>
-        {displayProducts}
-				</div>
-				<Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-title"
-        aria-describedby="modal-description"
-      >
-        <div className="modal-content scrolleable">
-          {selectedProduct && (
-            <>
-						<div className="row">
-              <h2 id="modal-title">{selectedProduct.descripcion}</h2>
-						</div>
-              <img className='img-producto marginr marginl' src={`https://drive.google.com/uc?export=view&id=${selectedProduct.id}`} alt="Imagen del producto" />
-              <p>Marca: {selectedProduct.marca}</p>
-							<p>Descripcion: {selectedProduct.descripcion}</p>
-              <p>Precio: ${selectedProduct.precio}.00 MXN</p>
-              <p>{selectedProduct.codigo} - {selectedProduct.contenido}</p>
-            </>
-          )}
-					<motion.button whileHover={{scale:1.1}} onClick={handleClose} whileTap={{scale:1.1}} className=" margint marginb button1 marginr marginl">Cerrar</motion.button>
+      </div>
+      <h1>{brandFilter ? brandFilter : "Todas las marcas"}</h1>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        placeholder="Buscar tus productos..."
+        className='buscador'
+      />
+      <div className="column">
+        <div className='boxVistaRapida marginr marginl'>
+          {displayProducts}
         </div>
-      </Modal>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
+        >
+          <div className="modal-content scrolleable">
+            {selectedProduct && (
+              <>
+                <div className="row">
+                  <h2 id="modal-title">{selectedProduct.descripcion}</h2>
+                </div>
+                <img className='img-producto marginr marginl' src={`https://drive.google.com/uc?export=view&id=${selectedProduct.id}`} alt="Imagen del producto" />
+                <p>Marca: {selectedProduct.marca}</p>
+                <p>Descripcion: {selectedProduct.descripcion}</p>
+                <p>Precio: ${selectedProduct.precio}.00 MXN</p>
+                <p>{selectedProduct.codigo} - {selectedProduct.contenido}</p>
+              </>
+            )}
+            <motion.button whileHover={{ scale: 1.1 }} onClick={handleClose} whileTap={{ scale: 1.1 }} className=" margint marginb button1 marginr marginl">Cerrar</motion.button>
+          </div>
+        </Modal>
 
-        <ReactPaginate
+        {/* <ReactPaginate
           previousLabel={<img src="https://cdn-icons-png.flaticon.com/128/151/151846.png" alt="Siguiente" className="siguiente" />}
           nextLabel={<img src="https://cdn-icons-png.flaticon.com/128/271/271228.png" alt="Siguiente" className="siguiente" />}
           previousClassName="boton-anterior"
@@ -213,10 +188,10 @@ export default function Productos() {
           containerClassName="pagination"
           subContainerClassName="pages pagination"
           activeClassName="active"
-          currentPageClassName="paginaAcutal"
+          currentPageClassName="paginaActual"
           pageRangeDisplayed={false}
           marginPagesDisplayed={false}
-        />
+        /> */}
       </div>
     </div>
   );
