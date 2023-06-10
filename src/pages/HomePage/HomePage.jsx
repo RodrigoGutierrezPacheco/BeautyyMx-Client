9aced8d548711b8bfe2b950761be4372c45e0d2c
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-06-10 14:34:31 -0600

    modificacion vista inigio

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,28 +7,6 @@ import { motion } from "framer-motion";
 
 function HomePage() {
  
-	const lanzamientos=[
-		{
-			marca:"Marca 1",
-			nombre:"Nombre 1",
-			precio:"$precio1.00MXN"
-		},
-		{
-			marca:"Marca 2",
-			nombre:"Nombre 2",
-			precio:"$precio2.00MXN"
-		},
-		{
-			marca:"Marca 3",
-			nombre:"Nombre 3",
-			precio:"$precio3.00MXN"
-		},
-		{
-			marca:"Marca 4",
-			nombre:"Nombre 4",
-			precio:"$precio4.00MXN"
-		}
-]
 
  function productos(){
 	window.location.href="/todos-los-productos"
@@ -74,55 +52,40 @@ function HomePage() {
     </Carousel>
 		<div className="nosotros">
 			<h1 className="title red width marginr marginl">Bienvenidos a Beautyymx, tu destino para el mejor maquillaje original en línea!</h1>
-			<h3 className="width marginr marginl margint">En Beautyymx, nos enorgullece ofrecerte una amplia selección de productos de maquillaje auténtico y de alta calidad. Nuestro objetivo es brindarte una experiencia de compra excepcional, donde puedas encontrar los productos que realcen tu belleza y te hagan sentir segura y empoderada.
+			<h3 className="bienvenida marginr marginl margint">En Beautyymx, nos enorgullece ofrecerte una amplia selección de productos de maquillaje auténtico y de alta calidad. Nuestro objetivo es brindarte una experiencia de compra excepcional, donde puedas encontrar los productos que realcen tu belleza y te hagan sentir segura y empoderada.
+			<br className="marginb" />
+			<br />
 			Explora nuestra tienda en línea y descubre marcas reconocidas y populares, así como productos exclusivos que no encontrarás en ningún otro lugar. Trabajamos directamente con proveedores confiables para garantizar la autenticidad de todos nuestros productos.
+			<br />
+			<br />
 			¡Gracias por visitar Beautyymx! Disfruta de tu experiencia de compra y encuentra los productos de maquillaje original que te harán lucir radiante.</h3>
 			<motion.button onClick={puntosDeVenta} whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button mb">Puntos de Venta</motion.button>
 		</div>
 		<div className="lanzamientos">
-			<h1 className="title">Recién Llegados</h1>
+			<h1 className="title">Las Mejores Marcas</h1>
 			<div className='boxVistaRapida1'>
 			<Carousel interval={3000} indicators={false} variant="dark">
-			{lanzamientos.map((lanzamiento)=>
 				<Carousel.Item >
 					<div className="boxProduct1">
-					 <img className="producto1" src="images/product1.jpg" alt="" />
-					 <h1>{lanzamiento.marca}</h1>
-					 <h3>NOMBRE DEL PRODUCTO</h3>
-					 <h3 className="precio">$799.00MXN</h3>
-					 <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button>
+					 <img className="marca" src="images/marca1.png" alt="" />
+					 {/* <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button> */}
 					</div>
 				</Carousel.Item>
-       )}
+				<Carousel.Item >
+					<div className="boxProduct1">
+					 <img className="marca" src="images/marca2.png" alt="" />
+					 {/* <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button> */}
+					</div>
+				</Carousel.Item>
+				<Carousel.Item >
+					<div className="boxProduct1">
+					 <img className="marca" src="images/marca3.png" alt="" />
+					 {/* <motion.button whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1">Ver Producto</motion.button> */}
+					</div>
+				</Carousel.Item>
      </Carousel>
 		 {/* <motion.button  whileTap={{scale:1.2}} whileHover={{scale:1.1}} className="button1 mt" >Ver Todo en Tamaño Viaje</motion.button> */}
 		 </div>
-		 {/* <div>
-			<h1 className="title mt1">CATALOGO POR CATEGORÍAS</h1>
-			<Carousel indicators={false} variant="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="images/inicio.png"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="images/productos.png"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="images/puntosVenta.png"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-		 </div> */}
 		</div>
 		</div>
     
