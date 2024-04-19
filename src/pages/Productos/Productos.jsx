4c909921708bd0991aef708d95806aeb8622594b
RodrigoGutierrezPacheco <rodrigogutierrezpacheco@gmail.com>
2024-04-18 19:00:30 -0600

    implementacion paypal

--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -16,6 +16,8 @@ import ReactPaginate from 'react-paginate'
 import Alert from 'react-bootstrap/Alert'
 import Swal from 'sweetalert2'
 import LazyLoad from 'react-lazyload'
+import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js'
+import { CoPresentOutlined } from '@mui/icons-material'
 
 export default function Productos() {
   const [imageLoading, setImageLoading] = useState(true)
@@ -29,6 +31,7 @@ export default function Productos() {
   const [cartItems, setCartItems] = useState([])
   const [showAlert, setShowAlert] = useState(false)
   const [loadingImages, setLoadingImages] = useState(true)
+  const [paypalAmount, setPaypalAmount] = useState(0);
 
   const handleImageLoad = () => {
     setImageLoading(false)
@@ -162,7 +165,6 @@ export default function Productos() {
     })
 
     message += `Precio total de todos los productos: $${total}.00 MXN`
-
     return message
   }
 
@@ -190,15 +192,15 @@ export default function Productos() {
 
   const filteredProducts = data
     ? data.filter(item => {
-        const lowerCaseSearchTerm = searchTerm.toLowerCase()
-        const lowerCaseBrandFilter = brandFilter.toLowerCase()
+      const lowerCaseSearchTerm = searchTerm.toLowerCase()
+      const lowerCaseBrandFilter = brandFilter.toLowerCase()
 
-        const matchesSearchTerm = item.descripcion && item.descripcion.toLowerCase().includes(lowerCaseSearchTerm)
-        const matchesBrandFilter =
-          (item.marca && item.marca.toLowerCase() === lowerCaseBrandFilter) || lowerCaseBrandFilter === ''
+      const matchesSearchTerm = item.descripcion && item.descripcion.toLowerCase().includes(lowerCaseSearchTerm)
+      const matchesBrandFilter =
+        (item.marca && item.marca.toLowerCase() === lowerCaseBrandFilter) || lowerCaseBrandFilter === ''
 
-        return matchesSearchTerm && matchesBrandFilter
-      })
+      return matchesSearchTerm && matchesBrandFilter
+    })
     : []
 
   const pageCountFiltered = Math.ceil(filteredProducts.length / filteredProducts.length)
@@ -252,6 +254,65 @@ export default function Productos() {
       </motion.button>
     </div>
   ))
+
+  const createWhatsAppMessage = (orderDetails) => {
+    let message = `¡Hola! Mi nombre es ${orderDetails.name} y acabo de realizar una compra en tu tienda. Aquí está el resumen de la compra:\n`;
+
+    // Agrega detalles de cada producto comprado
+    orderDetails.products.forEach(item => {
+      message += `- ${item.descripcion}: Cantidad: ${item.quantity}, Precio por unidad: $${item.precio}.00 MXN\n`;
+    });
+
+    // Agrega el total de la compra
+    message += `Total de la compra: $${orderDetails.totalAmount}.00 MXN\n`;
+
+    // Agrega la dirección de envío
+    message += `Dirección de envío: ${orderDetails.calle}, ${orderDetails.coloniadelegacion}, ${orderDetails.municipio}, ${orderDetails.estado}, CP: ${orderDetails.cp}\n`;
+
+    // Agrega el ID de transacción de PayPal
+    message += `ID de transacción de PayPal: ${orderDetails.orderId}\n`;
+
+    return message;
+  };
+
+  const handlePaypalPurchase = (orderDetails) => {
+    // Lógica para procesar la compra con PayPal
+
+    // Muestra una modal para confirmar el envío del mensaje de WhatsApp
+    Swal.fire({
+      title: 'Compra realizada con exito!',
+      text: 'Se enviará un mensaje a WhatsApp para confirmar tu compra. ¿Deseas continuar?',
+      icon: 'success',
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Una vez que el usuario acepta, construye el mensaje para WhatsApp
+        const message = createWhatsAppMessage(orderDetails);
+
+        // Genera el enlace de WhatsApp
+        const whatsappLink = `https://wa.me/525638686467?text=${encodeURIComponent(message)}`;
+
+        // Redirige al usuario al enlace de WhatsApp
+        window.open(whatsappLink, '_blank');
+
+        // Show a button in the modal to send the WhatsApp message
+        // Swal.fire({
+        //   title: 'Pago Completado!',
+        //   text: `Gracias por tu pago ${orderDetails.name}, en breve recibirás un correo a ${orderDetails.email} con la información solicitada y otro correo con la información de pago de PayPal`,
+        //   showCancelButton: true,
+        //   cancelButtonText: 'No, gracias',
+        //   confirmButtonText: 'Enviar lista por WhatsApp',
+        //   showLoaderOnConfirm: true,
+        //   preConfirm: () => {
+        //     // Aquí podrías abrir el modal para enviar la información a WhatsApp
+        //     handleOpenWhatsAppModal();
+        //   }
+        // });
+      }
+    });
+  };
 
   return (
     <div>
@@ -354,14 +415,6 @@ export default function Productos() {
                 </div>
               </>
             )}
-            <motion.button
-              whileHover={{ scale: 1.1 }}
-              onClick={handleClose}
-              whileTap={{ scale: 1.1 }}
-              className=" margint marginb button1 marginr marginl"
-            >
-              Cerrar
-            </motion.button>
           </div>
         </Modal>
 
@@ -421,16 +474,89 @@ export default function Productos() {
                   <h2>Total:</h2>
                   <p>${cartItems.reduce((total, item) => total + item.precio * item.quantity, 0)}.00 MXN</p>
                 </div>
-                <motion.button
+
+                {/* <motion.button
                   whileHover={{ scale: 1.1 }}
                   onClick={handleListSubmission}
                   whileTap={{ scale: 1.1 }}
                   className="margint marginb button1 marginr marginl"
                 >
                   Comprar
-                </motion.button>
+                </motion.button> */}
               </>
             )}
+            <PayPalScriptProvider options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID_TEST, components: "buttons", currency: "MXN", locale: "es_MX" }}>
+              <PayPalButtons
+                createOrder={(data, actions) => {
+                  return actions.order.create({
+                    purchase_units: [
+                      {
+                        amount: {
+                          value: cartItems.reduce((total, item) => total + item.precio * item.quantity, 0),
+                          currency_code: "MXN",
+                        },
+
+                        name: "Reto 1",
+                        description: "Reto que te ayudara a cumplir todas tus metas del mundo",
+                      },
+                    ],
+                    application_context: {
+                      brand_name: "Keto Challenge",
+                      name: "Reto 1",
+                      description: "Reto 1 que ye ayudará a cumplir todas tus metas",
+                      landing_page: "NO_PREFERENCE",
+                      user_action: "PAY_NOW",
+                    }
+                  });
+                }}
+                onApprove={(data, actions) => {
+                  return actions.order.capture().then((details) => {
+                    const orderDetails = {
+                      orderId: details.id,
+                      name: details.payer.name.given_name,
+                      lastName: details.payer.name.surname,
+                      email: details.payer.email_address,
+                      shippingAddress: details.purchase_units[0].shipping.address,
+                      calle: details.purchase_units[0].shipping.address?.address_line_1,
+                      coloniadelegacion: details.purchase_units[0].shipping.address?.address_line_2,
+                      estado: details.purchase_units[0].shipping.address?.admin_area_1,
+                      municipio: details.purchase_units[0].shipping.address?.admin_area_2,
+                      cp: details.purchase_units[0].shipping.address?.postal_code,
+                      products: cartItems,
+                      numeroDeProductos: cartItems?.length,
+                      productos: cartItems.map((producto) => { return producto?.codigo }),
+                      totalAmount: cartItems.reduce((total, item) => total + item.precio * item.quantity, 0),
+                    };
+                    console.log(orderDetails)
+                    handlePaypalPurchase(orderDetails);
+                    const name = details.payer.name.given_name;
+                    const orderId = details.id;
+                    const email = details.payer.email_address;
+                    // Swal.fire({
+                    //   icon: 'success',
+                    //   title: 'Pago Completado!',
+                    //   text: `Gracias por tu pago ${name}, en breve recibirás un correo a ${email} con la información solicitada y otro correo con la información de pago de PayPal`,
+                    //   confirmButtonColor: '#3085d6',
+                    // })
+                    // emailjs.send("service_n648gkj", "template_0wzy1rm", {
+                    //   user_name: details.payer.name,
+                    //   from: "ketochallengecuerna@gmail.com",
+                    //   user_email: details.payer.email_address,
+                    //   message: "confirmacion de compra",
+                    // }, process.env.REACT_APP_EMAILJS_PUBLICKEY);
+
+                  })
+                }}
+                onCancel={() => {
+                  Swal.fire({
+                    icon: 'error',
+                    title: 'Pago Cancelado',
+                    text: 'Se ha cancelado la solicitud con exito',
+                    confirmButtonColor: '#3085d6',
+                  })
+                }}
+              />
+            </PayPalScriptProvider>
             <motion.button
               whileHover={{ scale: 1.1 }}
               onClick={handleCartClose}
