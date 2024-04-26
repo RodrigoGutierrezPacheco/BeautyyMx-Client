efd4bd13a270a09b5049d42849e71093c70bd8f2
RodrigoGutierrezPacheco <rodrigogutierrezpacheco@gmail.com>
2024-04-25 18:58:47 -0600

    implementando envios de correos con confirmacion de compra

--- src/pages/FinalizarCompra/FinalizarCompra.jsx
+++ src/pages/FinalizarCompra/FinalizarCompra.jsx
@@ -1,10 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js'
 import Swal from 'sweetalert2'
-
-
+import emailjs from '@emailjs/browser';
 
 export default function FinalizarCompra() {
+    const form = useRef();
+
+    // const sendEmail = (e) => {
+    //     console.log("entrando funcion")
+    //     // e.preventDefault();
+    //     Swal.fire({
+    //         icon: 'success',
+    //         title: '¡Pago realizado con exito!',
+    //         text: 'En breve nos pondremos en contacto contigo',
+    //         confirmButtonColor: '#3085d6',
+    //     })
+    //     emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICEID, process.env.REACT_APP_EMAILJS_TEMPLATE, form.current, process.env.REACT_APP_EMAILJS_PUBLICKEY)
+    //         .then((result) => {
+    //             console.log(result)
+    //             Swal.fire({
+    //                 icon: 'success',
+    //                 title: '¡Correo Enviado!',
+    //                 text: 'En breve nos pondremos en contacto contigo',
+    //                 confirmButtonColor: '#3085d6',
+    //             })
+    //             setTimeout(function () {
+    //                 window.location.reload();
+    //             }, 5000);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
+
+
     const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
     const [paypalAmount, setPaypalAmount] = useState(0);
@@ -34,42 +63,44 @@ export default function FinalizarCompra() {
     };
 
     const handlePaypalPurchase = (orderDetails) => {
+        console.log("Pago confirmado")
+        // sendEmail()
         // Lógica para procesar la compra con PayPal
 
         // Muestra una modal para confirmar el envío del mensaje de WhatsApp
-        Swal.fire({
-            title: 'Compra realizada con exito!',
-            text: 'Se enviará un mensaje a WhatsApp para confirmar tu compra. ¿Deseas continuar?',
-            icon: 'success',
-            showCancelButton: true,
-            confirmButtonText: 'Aceptar',
-            cancelButtonText: 'Cancelar'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Una vez que el usuario acepta, construye el mensaje para WhatsApp
-                const message = createWhatsAppMessage(orderDetails);
+        // Swal.fire({
+        //     title: 'Compra realizada con exito!',
+        //     text: 'Se enviará un mensaje a WhatsApp para confirmar tu compra. ¿Deseas continuar?',
+        //     icon: 'success',
+        //     showCancelButton: true,
+        //     confirmButtonText: 'Aceptar',
+        //     cancelButtonText: 'Cancelar'
+        // }).then((result) => {
+        //     if (result.isConfirmed) {
+        //         // Una vez que el usuario acepta, construye el mensaje para WhatsApp
+        //         const message = createWhatsAppMessage(orderDetails);
 
-                // Genera el enlace de WhatsApp
-                const whatsappLink = `https://wa.me/525638686467?text=${encodeURIComponent(message)}`;
+        //         // Genera el enlace de WhatsApp
+        //         const whatsappLink = `https://wa.me/525638686467?text=${encodeURIComponent(message)}`;
 
-                // Redirige al usuario al enlace de WhatsApp
-                window.open(whatsappLink, '_blank');
+        //         // Redirige al usuario al enlace de WhatsApp
+        //         window.open(whatsappLink, '_blank');
 
-                // Show a button in the modal to send the WhatsApp message
-                // Swal.fire({
-                //   title: 'Pago Completado!',
-                //   text: `Gracias por tu pago ${orderDetails.name}, en breve recibirás un correo a ${orderDetails.email} con la información solicitada y otro correo con la información de pago de PayPal`,
-                //   showCancelButton: true,
-                //   cancelButtonText: 'No, gracias',
-                //   confirmButtonText: 'Enviar lista por WhatsApp',
-                //   showLoaderOnConfirm: true,
-                //   preConfirm: () => {
-                //     // Aquí podrías abrir el modal para enviar la información a WhatsApp
-                //     handleOpenWhatsAppModal();
-                //   }
-                // });
-            }
-        });
+        //         // Show a button in the modal to send the WhatsApp message
+        //         Swal.fire({
+        //             title: 'Pago Completado!',
+        //             text: `Gracias por tu pago ${orderDetails.name}, en breve recibirás un correo a ${orderDetails.email} con la información solicitada y otro correo con la información de pago de PayPal`,
+        //             showCancelButton: true,
+        //             cancelButtonText: 'No, gracias',
+        //             confirmButtonText: 'Enviar lista por WhatsApp',
+        //             showLoaderOnConfirm: true,
+        //             preConfirm: () => {
+        //                 // Aquí podrías abrir el modal para enviar la información a WhatsApp
+        //                 handleOpenWhatsAppModal();
+        //             }
+        //         });
+        //     }
+        // });
     };
 
     return (
@@ -138,23 +169,55 @@ export default function FinalizarCompra() {
                                 productos: cartItems.map((producto) => { return producto?.codigo }),
                                 totalAmount: cartItems.reduce((total, item) => total + item.precio * item.quantity, 0),
                             };
-                            console.log(orderDetails)
                             handlePaypalPurchase(orderDetails);
                             const name = details.payer.name.given_name;
                             const orderId = details.id;
                             const email = details.payer.email_address;
-                            // Swal.fire({
-                            //     icon: 'success',
-                            //     title: 'Pago Completado!',
-                            //     text: `Gracias por tu pago ${name}, en breve recibirás un correo a ${email} con la información solicitada y otro correo con la información de pago de PayPal`,
-                            //     confirmButtonColor: '#3085d6',
-                            // })
-                            // emailjs.send("service_n648gkj", "template_0wzy1rm", {
-                            //   user_name: details.payer.name,
-                            //   from: "ketochallengecuerna@gmail.com",
-                            //   user_email: details.payer.email_address,
-                            //   message: "confirmacion de compra",
-                            // }, process.env.REACT_APP_EMAILJS_PUBLICKEY);
+                            const params = {
+                                user_name: orderDetails?.name,
+                                from: "ketochallengecuerna@gmail.com",
+                                userEmail: "gutierrezpachecorodrigo@gmail.com",
+                                user_orderId: orderDetails?.orderId,
+                                products: orderDetails.products.map(product => ({
+                                    name: product.descripcion,
+                                    quantity: product.quantity,
+                                    price: product.precio,
+                                    image: product.fotos
+                                })),
+                                totalAmount: orderDetails.totalAmount
+                            }
+                            emailjs.send(process.env.REACT_APP_EMAILJS_SERVICEID, process.env.REACT_APP_EMAILJS_TEMPLATE, params, process.env.REACT_APP_EMAILJS_PUBLICKEY).then(
+                                (response) => {
+                                    console.log('SUCCESS!', response.status, response.text);
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Pago Completado!',
+                                        text: `Gracias por tu pago ${name}, en breve recibirás un correo a ${email} con la información de tu compra.`,
+                                        confirmButtonColor: '#3085d6',
+                                        allowOutsideClick: false, // Evita que se cierre haciendo clic fuera del modal
+                                        allowEscapeKey: false, // Evita que se cierre al presionar la tecla "Esc"
+                                    })
+                                },
+                                (error) => {
+                                    console.log('FAILED...', error);
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Pago Completado!',
+                                        text: `Gracias por tu pago ${name}, por desgracia el sistema no podrá enviarte un correo electrónico con los detalles de tu compra, pero aquí tienes tu ticket.`,
+                                        allowOutsideClick: false, // Evita que se cierre haciendo clic fuera del modal
+                                        allowEscapeKey: false, // Evita que se cierre al presionar la tecla "Esc"
+                                        confirmButtonText: 'Descargar',
+                                        confirmButtonColor: '#3085d6',
+                                    }).then((result) => {
+                                        if (result.isConfirmed) {
+                                            window.alert('Descargando');
+                                            // Aquí puedes agregar el código para descargar el ticket
+                                        }
+                                    });
+
+
+                                },
+                            );
 
                         })
                     }}
